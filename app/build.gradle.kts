3c8f3479740c601342ec89cc9bdc5f1f286bc1c5
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-10-12 17:02:38 +0200

    primero

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.promul_intent_implicitos"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.promul_intent_implicitos"
--- app/src/main/java/com/example/promul_intent_implicitos/MainActivity.java
+++ app/src/main/java/com/example/promul_intent_implicitos/MainActivity.java
@@ -2,13 +2,102 @@ package com.example.promul_intent_implicitos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textoWeb;
+    TextView textoMap;
+    TextView textoCopiar;
+
+    Button botonWeb;
+    Button botonMap;
+    Button botonCopiar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textoWeb = (TextView) findViewById(R.id.textoWeb);
+        textoMap = (TextView) findViewById(R.id.textoMap);
+        textoCopiar = (TextView) findViewById(R.id.textoCopiar);
+
+        botonWeb = (Button) findViewById(R.id.botonWeb);
+        botonMap = (Button) findViewById(R.id.botonMap);
+        botonCopiar = (Button) findViewById(R.id.botonCopiar);
+
+        botonWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirNavegador();
+            }
+        });
+
+
+        botonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        botonCopiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+    private void abrirNavegador() {
+        String url = textoWeb.getText().toString();
+
+        if(url.matches("^(https?|ftp)://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(:(\\d{1,5}))?(/\\S*)?$")){
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        }else {
+            textoWeb.setError("La dirección introducida no es válida");
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
