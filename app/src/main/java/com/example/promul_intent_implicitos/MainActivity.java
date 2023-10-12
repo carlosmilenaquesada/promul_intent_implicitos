5163fdc8a62b5529ee28115e66b882208ff966e9
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-10-12 17:28:58 +0200

    primero

--- app/src/main/java/com/example/promul_intent_implicitos/MainActivity.java
+++ app/src/main/java/com/example/promul_intent_implicitos/MainActivity.java
@@ -20,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
     Button botonMap;
     Button botonCopiar;
 
+    /*
+     *Los intent implícitos no son ejecutados por un programa concreto, sino que muestran una
+     * lista de programas disponibles que pueden ejecutar la acción del intent (estos programas deben
+     * estar instalados en nuestro dispositivo).
+     *
+     * Por ejemplo, abrir una dirección web puede hacerlos chrome, opera, firefox, etc. así que si
+     * configuramos el intent como Intent.ACTION_VIEW, al ejecutarlo, podremos elegir cual programa lo abrirá.
+     *
+     * Existen diversos tipos de intent de esta naturaleza, por ejemplo Intent.ACTION_VIEW, es para direcciones
+     * de páginas web, Intent.ACTION_SEND para mandar correos electrónicos, para mandar un texto a una aplicacion se usa
+     * ShareCompat.IntentBuilder, etc.
+     * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         botonWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                abrirNavegador();
+                abrirNavegadorWeb();
             }
         });
 
@@ -43,32 +55,59 @@ public class MainActivity extends AppCompatActivity {
         botonMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                abrirMapa();
             }
         });
 
         botonCopiar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                copiarTexto();
             }
         });
 
 
     }
 
-    private void abrirNavegador() {
-        String url = textoWeb.getText().toString();
-
-        if(url.matches("^(https?|ftp)://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(:(\\d{1,5}))?(/\\S*)?$")){
+    /*
+    * Intent para abrir una dirección web proporcionada por el usuario. Tendremos que tomar la
+    * dirección de un textfield, comprobar que es una web válida, configurar el intent y finalmente
+    * lanzarlo
+    * */
+    private void abrirNavegadorWeb() {
+        String url = String.valueOf(textoWeb.getText());
+        String regex = "^(https?|ftp)://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(:(\\d{1,5}))?(/\\S*)?$";
+        if (url.matches(regex)) {
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse(url));
             startActivity(intent);
-        }else {
+        } else {
             textoWeb.setError("La dirección introducida no es válida");
         }
+    }
+
+    /*
+     * Intent para abrir una localización en mapa dada por el usuario. Tendremos que tomar la
+     * localizacion de un textfield, configurar el intent y finalmente lanzarlo.
+     *
+     * Es igual que en el caso de la dirección web, pero con un añadido en el intent.setData();
+     * */
+    private void abrirMapa() {
+        String localizacion = String.valueOf(textoMap.getText());
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q=" + localizacion));
+        startActivity(intent);
+    }
+
+    /*
+    * Intent para llevar un texto a una aplicación, o al portapapeles.
+    * */
+
+    private void copiarTexto(){
+
 
     }
+
 }
 
 
